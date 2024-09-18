54719613eed938ee8ccf4f0189ce78a75e804154
E. Boganov <jenyaz862@gmail.com>
2024-09-18 18:19:28 +0300

    finish 42:51

--- react_test/src/App.test.js
+++ react_test/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+
+    const HelloWorldElemnt = screen.getByText(/Hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input/i);
+    expect(HelloWorldElemnt).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+  test("second", () => {
+    render(<App />);
+
+    const HelloWorldElemnt = screen.getByText(/Hello world/i);
+  });
 });
