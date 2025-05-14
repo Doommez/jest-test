8b9a6a8063981fda8dbc169cf58f173b598e3fe6
E. Boganov <jenyaz862@gmail.com>
2025-05-14 10:45:59 +0300

    feat: add helper renderWithStore.js

--- react_test/src/components/counter/Counter.test.tsx
+++ react_test/src/components/counter/Counter.test.tsx
@@ -3,14 +3,11 @@ import Counter from "./Counter";
 import userEvent from "@testing-library/user-event";
 import {Provider} from "react-redux";
 import { createReduxStore } from "src/store/store";
+import { renderWithStore } from "src/tests/helpers/renderWithStore";
 
 describe('Counter', () => {
     test('renders correctly', async () => {
-        const counter = render(
-            <Provider store={createReduxStore()}>
-                <Counter />
-            </Provider>
-            )
+        const counter = renderWithStore(<Counter />, {counter: {value: 0}});
         counter.getByTestId('increment')
         const incrementBtn = counter.getByTestId('increment')
         expect(counter.getByTestId('value-title')).toHaveTextContent('0');
